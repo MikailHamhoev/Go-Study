90796c52d67432bbe7f7817426454ac96cfd3045
Mikail Hamhoev <mikailhamhoev08@gmail.com>
2025-09-27 08:09:33 +0300

    Add constants.go

---
+++ types/constants.go
@@ -0,0 +1,11 @@
+package main
+
+// ======================================================
+// 1. BASIC CONSTANTS — UNTYPED & TYPED
+// ======================================================
+
+const Pi = 3.14159265358979323846 // untyped constant — flexible!
+const appName = "GoMaster"        // untyped string constant
+const debug = false               // untyped bool
+
+const maxRetries int = 3 // typed constant — enforced at compile time
