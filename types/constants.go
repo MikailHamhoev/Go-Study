e382d589732e3cd0efb196ae3482b28508a7fb4d
Mikail Hamhoev <mikailhamhoev08@gmail.com>
2025-10-07 07:45:28 +0300

    Add constant list with IOTA

--- types/constants.go
+++ types/constants.go
@@ -104,3 +104,12 @@ func (p Permission) Has(required Permission) bool {
 // ======================================================
 
 type HTTPMethod int
+
+const (
+	_      HTTPMethod = iota // skip 0
+	GET                      // 1
+	POST                     // 2
+	PUT                      // 3
+	DELETE                   // 4
+	PATCH                    // 5
+)
